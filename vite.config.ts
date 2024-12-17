6071d6585ba7fc9e3dda61b7e97527b4af4c2bc5
Lasse Munk <lm@it-kartellet.dk>
2024-12-17 17:01:04 +0100

    added redirects

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue({
       template: {
@@ -11,5 +12,13 @@ export default defineConfig({
         }
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      src: "/src/"
+    }
+  },
+  server: {
+    open: true
+  }
 });
